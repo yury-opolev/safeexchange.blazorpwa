4cd43554a380f48d50317b711dd8c5ca018e1c25
Yury Opolev <space.oyster@outlook.com>
2020-11-28 13:38:38 +0100

    Fixed issue #2

---
+++ SafeExchange.BlazorPWA/Classes/Model/NotificationData.cs
@@ -0,0 +1,41 @@
+﻿/// <summary>
+/// ...
+/// </summary>
+
+namespace SafeExchange.BlazorPWA.Model
+{
+    using System;
+
+    public class NotificationData
+    {
+        public NotificationType Type { get; set; }
+
+        public string Status { get; set; }
+
+        public string Message { get; set; }
+
+        public string GetAlertClass()
+        {
+            switch (this.Type)
+            {
+                case NotificationType.Primary:
+                    return "alert-primary";
+
+                case NotificationType.Secondary:
+                    return "alert-secondary";
+
+                case NotificationType.Success:
+                    return "alert-success";
+
+                case NotificationType.Warning:
+                    return "alert-warning";
+
+                case NotificationType.Danger:
+                    return "alert-danger";
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
--- SafeExchange.BlazorPWA/Classes/StateContainer.cs
+++ SafeExchange.BlazorPWA/Classes/StateContainer.cs
@@ -4,13 +4,28 @@
 
 namespace SafeExchange.BlazorPWA
 {
+    using SafeExchange.BlazorPWA.Model;
     using System;
 
     public class StateContainer
     {
         public string CurrentPageHeader { get; private set; }
 
+        public NotificationData Notification { get; private set; }
+
         public event Action OnChange;
+
+        public void SetNextNotification(NotificationData notification)
+        {
+            this.Notification = notification;
+        }
+
+        public NotificationData TakeNotification()
+        {
+            var result = this.Notification;
+            this.Notification = null;
+            return result;
+        }
 
         public void SetCurrentPageHeader(string pageHeader)
         {
