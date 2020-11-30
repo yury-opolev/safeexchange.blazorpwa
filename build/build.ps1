2a6e6572f8cbfa061f6138ecade947465d7abb5a
Yury Opolev <space.oyster@outlook.com>
2020-11-30 23:26:27 +0100

    Minor improvements

--- build/build.ps1
+++ build/build.ps1
@@ -9,9 +9,15 @@ $PropsFileName = "Directory.Build.props"
 $FileContent = Get-Content "$Env:ProjectRoot\build\templates\$PropsFileName.template" -Raw
 $FileContent -replace "{VERSION}","$Version" | Set-Content -Path "$Env:ProjectRoot\$PropsFileName"
 
+# clean output directory
+Remove-Item -Path "$Env:ProjectRoot\SafeExchange.BlazorPWA\bin\" -Recurse -ErrorAction Ignore
+
 # build and publish
 dotnet build --configuration Release
 dotnet publish --configuration Release
+
+# remove Directory.Build.props
+Remove-Item -Path "$Env:ProjectRoot\$PropsFileName"
 
 $PublishFolderName = "$Env:ProjectRoot\SafeExchange.BlazorPWA\bin\Release\netstandard2.1\publish\"
 
