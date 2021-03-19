40bc6dd8e62d3f3a85e397993d4789265f96b1e0
Yury Opolev <space.oyster@outlook.com>
2021-03-19 12:44:01 +0100

    Added missing injections

--- SafeExchange.Client.Web.Components/ServicesHelper.cs
+++ SafeExchange.Client.Web.Components/ServicesHelper.cs
@@ -27,6 +27,8 @@ namespace SafeExchange.Client.Web.Components
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddScoped<ClipboardService>();
+            builder.Services.AddScoped<PushNotifications>();
+            builder.Services.AddScoped<NotificationsSubscriber>();
 
             builder.Services.AddMsalAuthentication(options =>
             {
