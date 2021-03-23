72f22f550be74d9e4833757a11224782e9cccf3d
Yury Opolev <space.oyster@outlook.com>
2021-03-23 22:11:57 +0100

    Minor changes

--- SafeExchange.Client.Web.Components/Classes/Model/Input/AccessRequestDataInput.cs
+++ SafeExchange.Client.Web.Components/Classes/Model/Input/AccessRequestDataInput.cs
@@ -5,9 +5,13 @@
 namespace SafeExchange.Client.Web.Components.Model
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
 
     public class AccessRequestDataInput
     {
+        [Required]
+        [StringLength(63, ErrorMessage = "Name too long (63 character limit).")]
+        [RegularExpression(@"^[0-9a-zA-Z-]+$", ErrorMessage = "Only letters, numbers and hyphens are allowed.")]
         public string SecretId { get; set; }
 
         public string Permission { get; set; }
