6b2e6514c7989e67bdde9479a177c79f9c52253f
Yury Opolev <space.oyster@outlook.com>
2021-04-14 20:12:15 +0200

    Fix search text field

---
+++ SafeExchange.Client.Web.Components/Classes/Model/Input/SearchInput.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// ...
+/// </summary>
+
+namespace SafeExchange.Client.Web.Components.Model
+{
+    using System;
+
+    public class SearchInput
+    {
+        public string SearchText { get; set; }
+    }
+}
