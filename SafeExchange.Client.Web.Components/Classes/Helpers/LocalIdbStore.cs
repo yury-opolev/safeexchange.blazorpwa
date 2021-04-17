5a569de4e0d559e07b937e7396860a172c89a179
Yury Opolev <space.oyster@outlook.com>
2021-04-17 14:17:09 +0200

    Minor changes

---
+++ SafeExchange.Client.Web.Components/Classes/Helpers/LocalIdbStore.cs
@@ -0,0 +1,85 @@
+﻿/// <summary>
+/// ...
+/// </summary>
+
+namespace SafeExchange.Client.Web.Components.Classes.Helpers
+{
+    using Microsoft.JSInterop;
+    using SafeExchange.Client.Web.Components.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class LocalIdbStore : IAsyncDisposable
+    {
+        private const string StoreName = "accessRequests";
+
+        private readonly Lazy<Task<IJSObjectReference>> helperModuleTask;
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public LocalIdbStore(IJSRuntime jsRuntime)
+        {
+            helperModuleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import",
+                "./_content/SafeExchange.Client.Web.Components/idb-min.js").AsTask());
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import",
+                "./_content/SafeExchange.Client.Web.Components/localIdbStore.js").AsTask());
+        }
+
+        public async ValueTask<DateTime> GetAccessRequestsFetchTimeUtc(string userId)
+        {
+            var module = await moduleTask.Value;
+            var currentBundle = await module.InvokeAsync<AccessRequestsBundle>("get", StoreName, userId);
+            return currentBundle?.LastUpdated ?? DateTime.MinValue;
+        }
+
+        public async ValueTask<AccessRequestsBundle> GetAccessRequests(string userId)
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<AccessRequestsBundle>("get", StoreName, userId);
+        }
+
+        public async ValueTask PutAccessRequests(AccessRequestsBundle latestBundle)
+        {
+            if (latestBundle.LastUpdated == DateTime.MinValue)
+            {
+                latestBundle.LastUpdated = DateTime.UtcNow;
+            }
+
+            var module = await moduleTask.Value;
+            await module.InvokeAsync<NotificationSubscription>("put", StoreName, latestBundle.UserId, latestBundle);
+        }
+
+        public async ValueTask<bool> ClearAccessRequests(string userId)
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<bool>("delete", StoreName, userId);
+        }
+
+        public async ValueTask<List<AccessRequestsBundle>> GetAllAccessRequests()
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<List<AccessRequestsBundle>>("getAll", StoreName);
+        }
+
+        public async ValueTask<bool> ClearAllAccessRequests()
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<bool>("clear", StoreName);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+
+            if (helperModuleTask.IsValueCreated)
+            {
+                var helperModule = await helperModuleTask.Value;
+                await helperModule.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ SafeExchange.Client.Web.Components/wwwroot/localIdbStore.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    const localDbName = 'safeExchange';
+    const db = idb.openDB(localDbName, 1, {
+        upgrade(db) {
+            db.createObjectStore('accessRequests', { keyPath: 'userId' }).createIndex('lastUpdated', 'lastUpdated');
+        },
+    });
+
+    window.localIdbStore = {
+        get: async (storeName, key) => (await db).transaction(storeName).store.get(key),
+        getAll: async (storeName) => (await db).transaction(storeName).store.getAll(),
+        getFirstFromIndex: async (storeName, indexName, direction) => {
+            const cursor = await (await db).transaction(storeName).store.index(indexName).openCursor(null, direction);
+            return (cursor && cursor.value) || null;
+        },
+        put: async (storeName, key, value) => (await db).transaction(storeName, 'readwrite').store.put(value, key === null ? undefined : key),
+        putAllFromJson: async (storeName, json) => {
+            const store = (await db).transaction(storeName, 'readwrite').store;
+            JSON.parse(json).forEach(item => store.put(item));
+        },
+        delete: async (storeName, key) => (await db).transaction(storeName, 'readwrite').store.delete(key),
+        clear: async (storeName) => (await db).transaction(storeName, 'readwrite').store.clear(),
+        autocompleteKeys: async (storeName, text, maxResults) => {
+            const results = [];
+            let cursor = await (await db).transaction(storeName).store.openCursor(IDBKeyRange.bound(text, text + '\uffff'));
+            while (cursor && results.length < maxResults) {
+                results.push(cursor.key);
+                cursor = await cursor.continue();
+            }
+            return results;
+        }
+    };
+})();
