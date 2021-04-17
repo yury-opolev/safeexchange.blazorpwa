85e21c908c2756225ee03123f1378eb6ff3dbf7c
Yury Opolev <space.oyster@outlook.com>
2021-04-17 16:00:39 +0200

    Add badges to requests menu item

--- SafeExchange.Client.Web.Components/Classes/StateContainer.cs
+++ SafeExchange.Client.Web.Components/Classes/StateContainer.cs
@@ -6,6 +6,8 @@ namespace SafeExchange.Client.Web.Components
 {
     using SafeExchange.Client.Web.Components.Model;
     using System;
+    using System.Linq;
+    using System.Threading.Tasks;
 
     public class StateContainer
     {
@@ -14,6 +16,14 @@ namespace SafeExchange.Client.Web.Components
         public NotificationData Notification { get; private set; }
 
         public event Action OnChange;
+
+        public bool IsFetchingAccessRequests { get; set; }
+
+        public ApiAccessRequestsListReply CachedAccessRequests { get; set; }
+
+        public int IncomingAccessRequestsCount => this.CachedAccessRequests?.AccessRequests.Where(ar => (ar.RequestType == AccessRequestType.Incoming)).Count() ?? 0;
+
+        public int OutgoingAccessRequestsCount => this.CachedAccessRequests?.AccessRequests.Where(ar => (ar.RequestType == AccessRequestType.Outgoing)).Count() ?? 0;
 
         public void SetNextNotification(NotificationData notification)
         {
@@ -31,6 +41,23 @@ namespace SafeExchange.Client.Web.Components
         {
             CurrentPageHeader = pageHeader;
             NotifyStateChanged();
+        }
+
+        public async Task<ApiAccessRequestsListReply> TryFetchAccessRequestsAsync(ApiClient apiClient)
+        {
+            this.IsFetchingAccessRequests = true;
+            try
+            {
+                this.CachedAccessRequests = null;
+                this.CachedAccessRequests = await apiClient.GetAccessRequestsAsync();
+            }
+            finally
+            {
+                this.IsFetchingAccessRequests = false;
+                NotifyStateChanged();
+            }
+
+            return this.CachedAccessRequests;
         }
 
         private void NotifyStateChanged() => OnChange?.Invoke();
