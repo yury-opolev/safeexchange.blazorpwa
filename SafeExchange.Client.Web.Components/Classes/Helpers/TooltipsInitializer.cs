1499b92900e42eb25d8bac025025e998cecbfa0e
Yury Opolev <space.oyster@outlook.com>
2021-04-19 22:47:42 +0200

    Add tooltips

---
+++ SafeExchange.Client.Web.Components/Classes/Helpers/TooltipsInitializer.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// ...
+/// </summary>
+
+namespace SafeExchange.Client.Web.Components
+{
+    using Microsoft.JSInterop;
+    using System;
+    using System.Threading.Tasks;
+
+    public class TooltipsInitializer : IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+        public TooltipsInitializer(IJSRuntime jsRuntime)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+               "import", "./_content/SafeExchange.Client.Web.Components/tooltipsInitializer.js").AsTask());
+        }
+
+        public async Task InitializeTooltipsAsync(string initialText, string clickText)
+        {
+            var module = await moduleTask.Value;
+            await module.InvokeVoidAsync("addTooltips", initialText, clickText);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
--- SafeExchange.Client.Web.Components/ServicesHelper.cs
+++ SafeExchange.Client.Web.Components/ServicesHelper.cs
@@ -27,6 +27,7 @@ namespace SafeExchange.Client.Web.Components
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddScoped<ClipboardService>();
+            builder.Services.AddScoped<TooltipsInitializer>();
             builder.Services.AddScoped<PushNotifications>();
             builder.Services.AddScoped<NotificationsSubscriber>();
 
