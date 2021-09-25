731778b4597f055791d5bab6937c1610e01e5adb
Yury Opolev <yuri.opolev@gmail.com>
2021-09-25 23:24:24 +0200

    Minor changes

---
+++ SafeExchange.Client.Web.Components/Classes/NavLinkAccessibility.cs
@@ -0,0 +1,42 @@
+﻿/// <summary>
+/// ..
+/// </summary>
+
+namespace SafeExchange.Client.Web.Components
+{
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.AspNetCore.Components.Rendering;
+    using Microsoft.AspNetCore.Components.Routing;
+    using System.Collections.Generic;
+
+    public class NavLinkAccessibility : NavLink
+    {
+        /// <summary>
+        /// Gets or sets the attributes applied to the NavLink when the
+        /// current route matches the NavLink href.
+        /// </summary>
+        [Parameter]
+        public IReadOnlyDictionary<string, object>? ActiveAttributes { get; set; }
+
+        /// <inheritdoc/>
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, "a");
+
+            builder.AddMultipleAttributes(1, AdditionalAttributes);
+            builder.AddAttribute(2, "class", CssClass);
+
+            if (CssClass?.Contains("active") == true && ((ActiveAttributes?.Count ?? 0) > 0))
+            {
+                builder.AddMultipleAttributes(3, ActiveAttributes);
+                builder.AddContent(4, ChildContent);
+            }
+            else
+            {
+                builder.AddContent(3, ChildContent);
+            }
+
+            builder.CloseElement();
+        }
+    }
+}
