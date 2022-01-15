e9b67f3c9d5ecf83d9f09acc9994d320ad7ba4b6
Yury Opolev <yuri.opolev@gmail.com>
2022-01-15 10:50:33 +0100

    Added ps solution and project

---
+++ SafeExchange.Client.Powershell/SendGreetingCommand.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// TestCommandlet
+/// </summary>
+
+namespace SafeExchange.Client.Powershell
+{
+    using System.Management.Automation;
+
+    [Cmdlet(VerbsCommunications.Send, "Greeting")]
+    public class SendGreetingCommand : Cmdlet
+    {
+        // Declare the parameters for the cmdlet.
+        [Parameter(Mandatory = true)]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string name;
+
+        // Override the ProcessRecord method to process
+        // the supplied user name and write out a
+        // greeting to the user by calling the WriteObject
+        // method.
+        protected override void ProcessRecord()
+        {
+            WriteObject("Hello " + name + "!");
+        }
+    }
+}
