29878e974abbcdaf34d7d0d941ec1ef02341f783
Yury Opolev <space.oyster@outlook.com>
2022-01-17 00:05:12 +0100

    Minor changes

--- SafeExchange.Client.Powershell5/BaseSafeSecretCommand.cs
+++ SafeExchange.Client.Powershell5/BaseSafeSecretCommand.cs
@@ -5,17 +5,20 @@
 namespace SafeExchange.Client.Powershell5
 {
     using SafeExchange.Client.Common;
+    using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Management.Automation;
+    using System.Reflection;
 
     public class BaseSafeSecretCommand : Cmdlet
     {
-        public static readonly string DefaultTenantId = "1472703e-99d8-45ee-aeac-7ec3ae9ab104";
+        public static readonly string DefaultTenantId = "9188040d-6c67-4c5b-b112-36a304b66dad";
         
         public static readonly string DefaultClientId = "b75602e2-2c3b-446b-9c92-77021db634eb";
         
-        public static readonly string DefaultAuthority = "https://login.microsoftonline.com/1472703e-99d8-45ee-aeac-7ec3ae9ab104";
+        public static readonly string DefaultAuthority = "https://login.microsoftonline.com/9188040d-6c67-4c5b-b112-36a304b66dad";
 
         public static readonly string DefaultRedirectUri = "msalb75602e2-2c3b-446b-9c92-77021db634eb://auth";
 
@@ -36,6 +39,34 @@ namespace SafeExchange.Client.Powershell5
         public bool UseIntegratedWindowsAuth { get; set; }
 
         protected ApiClient apiClient;
+
+        private Assembly compilerServicesAssembly;
+
+        public BaseSafeSecretCommand() 
+            : base()
+        {
+            var compilerServiceAssemblyLocation = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "System.Runtime.CompilerServices.Unsafe.dll");
+            this.compilerServicesAssembly = Assembly.LoadFrom(compilerServiceAssemblyLocation);
+            ResolveEventHandler onAssemblyResolve = (sender, e) =>
+            {
+                if (e.Name.Equals("System.Runtime.CompilerServices.Unsafe, Version=4.0.4.1, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"))
+                {
+                    return this.compilerServicesAssembly;
+                }
+
+                foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    if (assembly.FullName.Equals(e.Name))
+                    {
+                        return assembly;
+                    }
+                }
+
+                return null;
+            };
+
+            AppDomain.CurrentDomain.AssemblyResolve += onAssemblyResolve;
+        }
 
         protected override void BeginProcessing()
         {
