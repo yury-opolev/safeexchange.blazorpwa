b7111a8efc504920ee285a9b2614e87351b84c9c
Yury Opolev <space.oyster@outlook.com>
2022-01-27 22:59:53 +0100

    Add first iteration

--- SafeExchange.Client.Web.Components/wwwroot/richTextEditor.js
+++ SafeExchange.Client.Web.Components/wwwroot/richTextEditor.js
@@ -1,4 +1,29 @@
 
+const EmbedBlot = Quill.import('blots/embed');
+
+class PasswordBlot extends EmbedBlot {
+    static create(value) {
+        const node = super.create(value);
+        node.setAttribute("data-value", value);
+
+        const innerSpan = document.createElement("span");
+        innerSpan.className = "border border-secondary border-2 rounded";
+        innerSpan.innerHTML = " ***** ";
+        node.appendChild(innerSpan);
+
+        return node;
+    }
+
+    static value(node) {
+        return node.getAttribute("data-value");
+    }
+}
+PasswordBlot.blotName = 'password';
+PasswordBlot.tagName = 'SPAN';
+PasswordBlot.className = "ql-password-holder";
+
+Quill.register(PasswordBlot, true);
+
 function initializeEditor(dotNetRef, quillElement, placeholder, readOnly, nextElement) {
     var options = {
         placeholder: placeholder,
@@ -9,15 +34,32 @@ function initializeEditor(dotNetRef, quillElement, placeholder, readOnly, nextEl
     if (readOnly) {
         var toolbarOptions = false;
     } else {
-        var toolbarOptions = [
-            [{ 'font': [] }],
-            [{ 'size': [] }],
-            ['bold', 'italic', 'underline', 'strike'],
-            [{ 'align': [] }],
-            [{ 'color': [] }, { 'background': [] }],
-            ['link', 'image'],
-            ['clean']
-        ];
+        var toolbarOptions = {
+            container: [
+                [{ 'font': [] }],
+                [{ 'size': [] }],
+                ['bold', 'italic', 'underline', 'strike'],
+                [{ 'align': [] }],
+                [{ 'color': [] }, { 'background': [] }],
+                ['link', 'image'],
+                ['password'],
+                ['clean']
+            ],
+            handlers: {
+                'password': function (value) {
+                    if (value) {
+                        var range = this.quill.getSelection();
+                        if (range == null || range.length === 0) {
+                            return;
+                        }
+                        var text = this.quill.getText(range);
+                        this.quill.deleteText(range.index, range.length)
+                        this.quill.insertEmbed(range.index, 'password', text, Quill.sources.API);
+                        this.quill.setSelection(range.index + 1, Quill.sources.API);
+                    }
+                }
+            }
+        };
     }
 
     var bindings = {
