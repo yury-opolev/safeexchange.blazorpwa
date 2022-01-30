31012b6274d5a4c704c98e68b0450cd7595dce86
Yury Opolev <yuri.opolev@gmail.com>
2022-01-30 12:32:41 +0100

    Minor changes

--- SafeExchange.Client.Web.Components/wwwroot/tooltipsInitializer.js
+++ SafeExchange.Client.Web.Components/wwwroot/tooltipsInitializer.js
@@ -8,4 +8,20 @@ function addTooltips(initialText, clickText) {
     });
 }
 
-export { addTooltips };
+function addQuillClipboardTooltips(quillElement, initialText, clickText) {
+    var tooltipTriggerList = quillElement.querySelectorAll('[data-bs-toggle="tooltip"]');
+    tooltipTriggerList.forEach((element) => {
+        const content = element.parentElement.parentElement.parentElement.getAttribute("data-value");
+        initClipboardTooltip(element, content, initialText, clickText);
+    });
+}
+
+function initClipboardTooltip(element, content, initialText, clickText) {
+    const tooltip = new bootstrap.Tooltip(element)
+    element.onclick = async () => {
+        await navigator.clipboard.writeText(content);
+        $(element).attr("title", clickText).tooltip("_fixTitle").tooltip("show").attr("title", initialText).tooltip("_fixTitle");
+    };
+}
+
+export { addTooltips, addQuillClipboardTooltips };
