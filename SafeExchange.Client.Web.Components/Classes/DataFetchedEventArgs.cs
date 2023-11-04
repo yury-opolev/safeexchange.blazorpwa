a73c761f2431a092e1c6483128c4e37bcafa151b
yury-opolev <75037545+yury-opolev@users.noreply.github.com>
2023-11-04 19:15:22 +0100

    Minor changes (#28)

---
+++ SafeExchange.Client.Web.Components/Classes/DataFetchedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// DataFetchedEventArgs
+/// </summary>
+
+namespace SafeExchange.Client.Web.Components.Classes
+{
+    using SafeExchange.Client.Common.Model;
+    using System;
+
+    public class DataFetchedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Data fetch response status.
+        /// </summary>
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+}
