7ef8d2cede94bfe9830af3d3e52ca55f7d7b1049
yury-opolev <space.oyster@outlook.com>
2024-01-14 10:36:40 +0100

    Minor changes

--- SafeExchange.Client.Web.Components/ServicesHelper.cs
+++ SafeExchange.Client.Web.Components/ServicesHelper.cs
@@ -5,14 +5,31 @@
 namespace SafeExchange.Client.Web.Components
 {
     using System;
+    using System.Collections.Generic;
+    using System.Net;
     using System.Net.Http;
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Polly;
     using SafeExchange.Client.Common;
 
     public class ServicesHelper
     {
+        private static readonly IList<HttpStatusCode> TransientErrorStatusCodes = new List<HttpStatusCode>()
+        {
+            HttpStatusCode.RequestTimeout,
+            HttpStatusCode.TooManyRequests,
+            HttpStatusCode.ServiceUnavailable,
+            HttpStatusCode.GatewayTimeout,
+        };
+
+        public static IAsyncPolicy<HttpResponseMessage> DefaultRetryPolicy
+            => Policy
+                .Handle<HttpRequestException>()
+                .OrResult<HttpResponseMessage>(msg => TransientErrorStatusCodes.Contains(msg.StatusCode))
+                .WaitAndRetryAsync(2, retryAttempt => TimeSpan.FromSeconds(retryAttempt));
+
         public static void ConfigureServices(WebAssemblyHostBuilder builder)
         {
             builder.Services.AddSingleton<StateContainer>();
@@ -25,7 +42,9 @@ namespace SafeExchange.Client.Web.Components
                     client.BaseAddress = new Uri(apiConfiguration["BaseAddress"]);
                     client.Timeout = TimeSpan.FromMinutes(3);
                 })
-                .AddHttpMessageHandler<ApiAuthorizationMessageHandler>();
+                .AddHttpMessageHandler<ApiAuthorizationMessageHandler>()
+                .AddPolicyHandler(DefaultRetryPolicy)
+                .SetHandlerLifetime(TimeSpan.FromMinutes(10));
 
             builder.Services.AddScoped<ApiClient>();
 
