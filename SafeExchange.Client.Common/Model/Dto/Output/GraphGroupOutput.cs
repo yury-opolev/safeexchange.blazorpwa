7e50342234110fede438b7e07578b74f4db2ce6c
yury-opolev <space.oyster@outlook.com>
2025-02-18 22:39:34 +0100

    Minor changes

--- SafeExchange.Client.Common/Model/Dto/Output/GraphGroupOutput.cs
+++ SafeExchange.Client.Common/Model/Dto/Output/GraphGroupOutput.cs
@@ -6,12 +6,22 @@ namespace SafeExchange.Client.Common.Model
 {
     using System;
 
-    public class GraphGroupOutput
+    public class GraphGroupOutput : IEquatable<GraphGroupOutput>
     {
         public string Id { get; set; }
 
         public string DisplayName { get; set; }
 
         public string? Mail { get; set; }
+
+        public bool Equals(GraphGroupOutput? other)
+        {
+            if (other == default)
+            {
+                return false;
+            }
+
+            return this.Id == other.Id;
+        }
     }
 }
