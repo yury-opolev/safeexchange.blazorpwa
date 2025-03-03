608df144efc3114ddef4c00f2aae4df746ea325c
yury-opolev <space.oyster@outlook.com>
2025-03-04 00:11:37 +0100

    Minor changes

---
+++ SafeExchange.Client.Web.Components/Classes/Model/CheckableEnum.cs
@@ -0,0 +1,13 @@
+﻿/// CheckableEnum
+
+namespace SafeExchange.Client.Web.Components.Classes.Model
+{
+    using System;
+
+    public class CheckableEnum<T> where T : struct, Enum
+    {
+        public bool IsSelected { get; set; }
+
+        public T Value { get; set; }
+    }
+}
